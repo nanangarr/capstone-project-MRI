8f00c9c02dc680c5a49324223fcb065a47698e15
Nanang Ardiansyah <nanangardiansyah@student.uns.ac.id>
2025-05-06 10:51:00 +0700

    update dikit

--- src/pages/users/history.jsx
+++ src/pages/users/history.jsx
@@ -2,9 +2,12 @@
 
 import React from "react";
 
-export default function History() {
-    return(
+export default function History({ collapsed }) {
+    return (
         <>
+            <div className={`container p-6 transition-all duration-300 ${collapsed ? 'max-w-[calc(100vw-4rem)]' : 'max-w-[calc(100vw-16rem)]'}`}>
+                <h1 className="font-bold text-2xl mb-6">Data Riwayat Prediksi</h1>
+            </div>
         </>
     )
 }
--- src/pages/users/pasien/index.jsx
+++ src/pages/users/pasien/index.jsx
@@ -2,9 +2,13 @@
 
 import React from "react";
 
-export default function DataPasien() {
-    return(
+export default function DataPasien({ collapsed }) {
+    return (
         <>
+            <div className={`container p-6 transition-all duration-300 ${collapsed ? 'max-w-[calc(100vw-4rem)]' : 'max-w-[calc(100vw-16rem)]'}`}>
+                <h1 className="font-bold text-2xl mb-6">Data Pasien</h1>
+
+            </div>
         </>
     )
 }
