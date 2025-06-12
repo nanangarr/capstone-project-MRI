3e0856b6c4697050f3cdecacd84cde71d05a46f2
Ricard <yamara670@gmail.com>
2025-06-12 16:33:31 +0700

    change the config

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://capstone-for-me-production-e843.up.railway.app/api/',
     headers: {
         'Content-Type': 'application/json',
     },
